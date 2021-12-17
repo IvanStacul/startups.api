d952091d5815759b85775d5037dea415c9208e3a
IvanStacul <stacul.ivan98@gmail.com>
2021-12-17 16:19:21 -0300

    fix bug

--- src/main/java/com/informatorio/startups/api/entity/User.java
+++ src/main/java/com/informatorio/startups/api/entity/User.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -78,7 +79,8 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public String getPassword() {
         return password;
     }
